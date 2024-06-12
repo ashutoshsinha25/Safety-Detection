49a4b9543e03fd625a88472120af5bd21a738989
ashutosh <ashutoshsinha519@gmail.com>
2024-06-12 17:02:53 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name='Industry-Safety-Detection',
+    version='0.0.1',
+    author='ashutosh',
+    author_email='ashutoshsinha519@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
