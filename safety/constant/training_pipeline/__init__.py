171c1827ca2206a0769229608e57326a8d887fab
ashutosh <ashutoshsinha519@gmail.com>
2024-06-16 01:08:39 +0530

    added data ingestion

---
+++ safety/constant/training_pipeline/__init__.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv 
+_ = load_dotenv() 
+
+
+AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_REGION_NAME = "us-east-1"
+
+
+
+ARTIFACTS_DIR: str = 'artifacts'
+
+
+
+# data ingestion related constant 
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_S3_DATA_NAME: str = "isd_data_mini.zip"
+DATA_BUCKET_NAME = "yolov7-safety"
